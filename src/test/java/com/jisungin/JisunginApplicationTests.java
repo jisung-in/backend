4073f285022f7e66f9c83420ebd4e555d9e4fd12
kimdohyung <pdohyung@gmail.com>
2024-03-10 16:42:30 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/jisungin/JisunginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisungin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JisunginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
