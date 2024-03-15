739c5dbbb4e55cc03a3f7c6081f748a0b193ef1e
jwooo <parkjwoo0515@gmail.com>
2024-03-15 21:03:34 +0900

    feat: GlobalExceptionHandler 로그 기능 추가 (#4)

--- src/main/java/com/jisungin/api/GlobalExceptionHandler.java
+++ src/main/java/com/jisungin/api/GlobalExceptionHandler.java
@@ -1,5 +1,10 @@
-package com.jisungin.exception;
+package com.jisungin.api;
 
+import com.jisungin.exception.BusinessException;
+import com.jisungin.exception.ErrorResponse;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -15,12 +21,19 @@ public class GlobalExceptionHandler {
         BindingResult bindingResult = e.getBindingResult();
         String firstErrorMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
 
+        List<String> errors = bindingResult.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        log.warn("MethodArgumentNotValidException: {}", errors);
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), firstErrorMessage));
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.info("BusinessException: {}", e.getMessage());
         return ResponseEntity.status(e.getCode()).body(ErrorResponse.of(e.getCode(), e.getMessage()));
     }
 
