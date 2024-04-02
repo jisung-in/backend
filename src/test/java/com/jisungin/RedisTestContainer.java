5a7f9715ec80a55548c5b434983ce561bc9edc03
jwooo <parkjwoo0515@gmail.com>
2024-04-02 11:37:25 +0900

    refactor: Redis password 추가 (#45)

--- src/test/java/com/jisungin/RedisTestContainer.java
+++ src/test/java/com/jisungin/RedisTestContainer.java
@@ -7,11 +7,13 @@ import org.testcontainers.containers.GenericContainer;
 public abstract class RedisTestContainer {
 
     static final String REDIS_IMAGE = "redis:6-alpine";
+    static final String REDIS_PASSWORD = "test_container_redis_password";
     static final GenericContainer<?> REDIS_CONTAINER;
 
     static {
         REDIS_CONTAINER = new GenericContainer<>(REDIS_IMAGE)
                 .withExposedPorts(6379)
+                .withEnv("REDIS_PASSWORD", REDIS_PASSWORD)
                 .withReuse(true);
         REDIS_CONTAINER.start();
     }
@@ -19,7 +21,8 @@ public abstract class RedisTestContainer {
     @DynamicPropertySource
     public static void overrideProps(DynamicPropertyRegistry registry) {
         registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
-        registry.add("spring.data.redis.port", () -> "" + REDIS_CONTAINER.getMappedPort(6379));
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
+        registry.add("spring.data.redis.password", () -> REDIS_CONTAINER);
     }
 
 }
