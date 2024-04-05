97c2cf8e837aca08944f2a238e15453e7358f36a
jwooo <parkjwoo0515@gmail.com>
2024-04-05 23:25:33 +0900

    refactor: QueryDslConfig JPQLTemplate 변경 (#58)

--- src/main/java/com/jisungin/config/QueryDslConfig.java
+++ src/main/java/com/jisungin/config/QueryDslConfig.java
@@ -1,5 +1,6 @@
 package com.jisungin.config;
 
+import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -14,7 +15,7 @@ public class QueryDslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(em);
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, em);
     }
 
 }
