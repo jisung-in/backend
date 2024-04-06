63f9f00d264666a3f731ef77aeec0012675f174c
jwooo <parkjwoo0515@gmail.com>
2024-04-06 21:30:59 +0900

    feat: 책 관련 토크룸 조회 및 페이징 쿼리 추가 (#58)

--- src/main/java/com/jisungin/domain/talkroom/repository/TalkRoomRepositoryCustom.java
+++ src/main/java/com/jisungin/domain/talkroom/repository/TalkRoomRepositoryCustom.java
@@ -3,10 +3,16 @@ package com.jisungin.domain.talkroom.repository;
 import com.jisungin.application.PageResponse;
 import com.jisungin.application.talkroom.response.TalkRoomFindAllResponse;
 import com.jisungin.application.talkroom.response.TalkRoomFindOneResponse;
+import com.jisungin.application.talkroom.response.TalkRoomQueryResponse;
+import java.util.List;
 
 public interface TalkRoomRepositoryCustom {
 
     PageResponse<TalkRoomFindAllResponse> findAllTalkRoom(long offset, int size, String order, String query);
+
+    List<TalkRoomQueryResponse> findTalkRoomsRelatedBook(String isbn, long offset, Integer size);
+
+    Long countTalkRoomsRelatedBook(String isbn);
 
     TalkRoomFindOneResponse findOneTalkRoom(Long talkRoomId);
 
