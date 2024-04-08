80851611d4df219008b42c468505a6d9e8984748
kimdohyung <pdohyung@gmail.com>
2024-04-08 22:48:28 +0900

    feat: 실시간 검색어 조회 API 구현 (#67)

---
+++ src/main/java/com/jisungin/api/search/SearchController.java
@@ -0,0 +1,32 @@
+package com.jisungin.api.search;
+
+import com.jisungin.api.ApiResponse;
+import com.jisungin.api.search.request.SearchKeywordRequest;
+import com.jisungin.application.search.SearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequestMapping("/v1/search")
+@RequiredArgsConstructor
+@RestController
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @PostMapping("/rank")
+    public ApiResponse<Void> search(@ModelAttribute SearchKeywordRequest request) {
+        log.info("키워드 = {}", request.getKeyword());
+        searchService.searchKeyword(request.getKeyword());
+        return ApiResponse.ok(null);
+    }
+
+    @GetMapping("/rank")
+    public ApiResponse<List<String>> search() {
+        return ApiResponse.ok(searchService.getRankKeywords());
+    }
+
+}
---
+++ src/main/java/com/jisungin/application/search/SearchService.java
@@ -0,0 +1,33 @@
+package com.jisungin.application.search;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class SearchService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public SearchService(@Qualifier("redisTemplateSecond") RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void searchKeyword(String keyword) {
+        ZSetOperations<String, String> zset = redisTemplate.opsForZSet();
+        zset.incrementScore("ranking", keyword, 1); // 점수 증가
+    }
+
+    public List<String> getRankKeywords() {
+        ZSetOperations<String, String> zset = redisTemplate.opsForZSet();
+        Set<String> typedTuples = zset.reverseRange("ranking", 0, 9);
+        return List.copyOf(typedTuples);
+    }
+
+}
