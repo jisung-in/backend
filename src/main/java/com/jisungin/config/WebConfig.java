ebf2fff1263a15b025922c344fe900d3b377bcbd
kimdohyung <pdohyung@gmail.com>
2024-04-14 13:13:45 +0900

    refactor: 프론트 요청 주소 https://api.jisungin.co.kr 로 수정 (#78)

--- src/main/java/com/jisungin/config/WebConfig.java
+++ src/main/java/com/jisungin/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://api.jisungin.co.kr")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
