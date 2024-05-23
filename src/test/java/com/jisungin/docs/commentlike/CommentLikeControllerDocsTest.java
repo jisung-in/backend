3282f6bdd27ac9b3b8f939d8222082789c7dfa7c
jwooo <parkjwoo0515@gmail.com>
2024-05-23 16:57:27 +0900

    test: CommentLike 조회 API Docs 테스트 추가 (#104)

--- src/test/java/com/jisungin/docs/commentlike/CommentLikeControllerDocsTest.java
+++ src/test/java/com/jisungin/docs/commentlike/CommentLikeControllerDocsTest.java
@@ -1,10 +1,16 @@
 package com.jisungin.docs.commentlike;
 
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -14,11 +20,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.jisungin.api.commentlike.CommentLikeController;
 import com.jisungin.application.commentlike.CommentLikeService;
+import com.jisungin.application.commentlike.response.CommentIds;
 import com.jisungin.docs.RestDocsSupport;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 public class CommentLikeControllerDocsTest extends RestDocsSupport {
@@ -31,10 +38,35 @@ public class CommentLikeControllerDocsTest extends RestDocsSupport {
     }
 
     @Test
+    @DisplayName("좋아요한 의견 ID 조회 API")
+    void findLikeCommentIds() throws Exception {
+        // given
+        given(commentLikeService.findCommentIds(anyLong()))
+                .willReturn(CommentIds.of(List.of(1L, 2L, 3L, 4L, 5L)));
+
+        // when // then
+        mockMvc.perform(get("/v1/comments/likes")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("commentlike/get-like",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(NUMBER).description("코드"),
+                                fieldWithPath("status").type(STRING).description("상태"),
+                                fieldWithPath("message").type(STRING).description("메세지"),
+                                fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                                fieldWithPath("data.commentIds").type(ARRAY).description("좋아요 누른 의견 ID")
+                        )
+                ));
+    }
+
+    @Test
     @DisplayName("의견 좋아요하는 API")
     void likeComment() throws Exception {
         mockMvc.perform(
-                        RestDocumentationRequestBuilders.post("/v1/comments/{commentId}/likes", "1")
+                        post("/v1/comments/{commentId}/likes", "1")
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
@@ -47,13 +79,13 @@ public class CommentLikeControllerDocsTest extends RestDocsSupport {
                                         .description("의견 ID")
                         ),
                         responseFields(
-                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                fieldWithPath("code").type(NUMBER)
                                         .description("코드"),
-                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                fieldWithPath("status").type(STRING)
                                         .description("상태"),
-                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                fieldWithPath("message").type(STRING)
                                         .description("메시지"),
-                                fieldWithPath("data").type(JsonFieldType.NULL)
+                                fieldWithPath("data").type(NULL)
                                         .description("return data null")
                         )
                 ));
@@ -63,7 +95,7 @@ public class CommentLikeControllerDocsTest extends RestDocsSupport {
     @DisplayName("의견 좋아요 취소하는 API")
     void unLikeComment() throws Exception {
         mockMvc.perform(
-                        RestDocumentationRequestBuilders.delete("/v1/comments/{commentId}/likes", "1")
+                        delete("/v1/comments/{commentId}/likes", "1")
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
@@ -76,13 +108,13 @@ public class CommentLikeControllerDocsTest extends RestDocsSupport {
                                         .description("의견 ID")
                         ),
                         responseFields(
-                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                fieldWithPath("code").type(NUMBER)
                                         .description("코드"),
-                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                fieldWithPath("status").type(STRING)
                                         .description("상태"),
-                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                fieldWithPath("message").type(STRING)
                                         .description("메시지"),
-                                fieldWithPath("data").type(JsonFieldType.NULL)
+                                fieldWithPath("data").type(NULL)
                                         .description("return data null")
                         )
                 ));
