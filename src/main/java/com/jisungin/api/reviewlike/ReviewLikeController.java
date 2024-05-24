58ac76bf89695c43a997683e45bbe42d1917b4c3
jwooo <parkjwoo0515@gmail.com>
2024-05-24 17:04:08 +0900

    feat: ReviewLike 조회 API 추가 (#104)

--- src/main/java/com/jisungin/api/reviewlike/ReviewLikeController.java
+++ src/main/java/com/jisungin/api/reviewlike/ReviewLikeController.java
@@ -2,9 +2,11 @@ package com.jisungin.api.reviewlike;
 
 import com.jisungin.api.ApiResponse;
 import com.jisungin.api.support.Auth;
+import com.jisungin.application.reviewlike.response.ReviewIds;
 import com.jisungin.application.reviewlike.ReviewLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewLikeController {
 
     private final ReviewLikeService reviewLikeService;
+
+    @GetMapping("/likes")
+    public ApiResponse<ReviewIds> findLikeReviewIds(@Auth Long userId) {
+        return ApiResponse.ok(reviewLikeService.findLikeReviewIds(userId));
+    }
 
     @PostMapping("/{reviewId}/likes")
     public ApiResponse<Void> likeReview(
--- src/main/java/com/jisungin/domain/reviewlike/repository/ReviewLikeRepository.java
+++ src/main/java/com/jisungin/domain/reviewlike/repository/ReviewLikeRepository.java
@@ -5,6 +5,7 @@ import com.jisungin.domain.reviewlike.ReviewLike;
 import com.jisungin.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,9 @@ import java.util.Optional;
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
 
     Optional<ReviewLike> findByUserAndReview(User user, Review review);
+
+    @Query("select rl.review.id from ReviewLike rl where rl.user.id = :userId")
+    List<Long> findReviewIdsByUserId(@Param("userId") Long userId);
 
     @Query("select rl.review.id from ReviewLike rl where rl.review.id in :reviewIds and rl.user.id = :userId")
     List<Long> findLikeReviewByReviewId(Long userId, List<Long> reviewIds);
