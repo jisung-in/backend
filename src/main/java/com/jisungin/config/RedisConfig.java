2994c825b571b9fe35063a3944f9b05150b5f9d4
jwooo <parkjwoo0515@gmail.com>
2024-06-05 17:31:13 +0900

    refactor: RedisConfig 설정 변경 (#115)

--- src/main/java/com/jisungin/config/RedisConfig.java
+++ src/main/java/com/jisungin/config/RedisConfig.java
@@ -1,22 +1,13 @@
 package com.jisungin.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -33,35 +24,19 @@ public class RedisConfig {
     @Value("${spring.data.redis.password}")
     private String password;
 
-    @Primary
-    @Bean(name = "redisConnectionFactoryFirst")
-    public LettuceConnectionFactory redisConnectionFactoryFirst() {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
 
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
         redisStandaloneConfiguration.setPassword(password);
-        redisStandaloneConfiguration.setDatabase(0);
 
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
-    @Bean(name = "redisConnectionFactorySecond")
-    public LettuceConnectionFactory redisConnectionFactorySecond() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
-        redisStandaloneConfiguration.setDatabase(1);
-
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
-    }
-
-    @Primary
-    @Bean(name = "redisTemplateFirst")
-    public RedisTemplate<String, String> redisTemplateFirst(
-            @Qualifier("redisConnectionFactoryFirst") LettuceConnectionFactory connectionFactory) {
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 
         redisTemplate.setConnectionFactory(connectionFactory);
@@ -76,34 +51,5 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean(name = "redisTemplateSecond")
-    public RedisTemplate<String, String> redisTemplateSecond(
-            @Qualifier("redisConnectionFactorySecond") LettuceConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
-        return redisTemplate;
-    }
-
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(
-            @Qualifier("redisConnectionFactoryFirst") RedisConnectionFactory connectionFactory) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        return container;
-    }
-
-    @Bean
-    public RedisCacheManager redisCacheManager(
-            @Qualifier("redisConnectionFactorySecond") RedisConnectionFactory connectionFactory) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-
-        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-    }
 }
 
