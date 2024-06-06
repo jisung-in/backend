3b0e7a32f3d386a9fa13d2cf633a9fe58c24203c
jwooo <parkjwoo0515@gmail.com>
2024-06-06 13:48:10 +0900

    test: RedisConfig 설정 변경으로 인한 테스트 수정 (#115)

--- src/test/java/com/jisungin/application/book/BestSellerServiceTest.java
+++ src/test/java/com/jisungin/application/book/BestSellerServiceTest.java
@@ -29,10 +29,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.test.context.event.RecordApplicationEvents;
 
 @SpringBootTest
-@RecordApplicationEvents
 public class BestSellerServiceTest extends RedisTestContainer {
 
     @Autowired
