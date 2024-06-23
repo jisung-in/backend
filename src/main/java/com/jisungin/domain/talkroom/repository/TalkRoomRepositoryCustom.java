4cfce158b69cf1c5fd181208eaefe77a62c99a70
jwooo <parkjwoo0515@gmail.com>
2024-06-23 22:07:54 +0900

    refactor: TalkRoomCustomRepository 리팩토링 (#124)

--- src/main/java/com/jisungin/domain/talkroom/repository/TalkRoomRepositoryCustom.java
+++ src/main/java/com/jisungin/domain/talkroom/repository/TalkRoomRepositoryCustom.java
@@ -1,25 +1,25 @@
 package com.jisungin.domain.talkroom.repository;
 
-import com.jisungin.application.talkroom.response.TalkRoomQueryResponse;
+import com.jisungin.application.talkroom.response.TalkRoomQueryEntity;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TalkRoomRepositoryCustom {
 
-    List<TalkRoomQueryResponse> findAllTalkRoom(Integer offset, Integer size, String order, String search, String day,
-                                                LocalDateTime now);
+    List<TalkRoomQueryEntity> findAllTalkRoom(Integer offset, Integer size, String order, String search, String day,
+                                              LocalDateTime now);
 
     Long countTalkRooms(String search, String day, LocalDateTime now);
 
-    List<TalkRoomQueryResponse> findTalkRoomsRelatedBook(String isbn, long offset, Integer size);
+    List<TalkRoomQueryEntity> findTalkRoomsRelatedBook(String isbn, long offset, Integer size);
 
     Long countTalkRoomsRelatedBook(String isbn);
 
-    TalkRoomQueryResponse findOneTalkRoom(Long talkRoomId);
+    TalkRoomQueryEntity findOneTalkRoom(Long talkRoomId);
 
-    List<TalkRoomQueryResponse> findByTalkRoomOwner(Integer offset, Integer size, boolean userTalkRoomsFilter,
-                                                    boolean commentFilter,
-                                                    boolean likeFilter, Long id);
+    List<TalkRoomQueryEntity> findByTalkRoomOwner(Integer offset, Integer size, boolean userTalkRoomsFilter,
+                                                  boolean commentFilter,
+                                                  boolean likeFilter, Long id);
 
     Long countTalkRoomsByUserId(Long userId, boolean userTalkRoomsFilter, boolean commentFilter, boolean likeFilter);
 
